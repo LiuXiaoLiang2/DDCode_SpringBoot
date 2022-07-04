8033332fdef57a5f37146e10ea2144e33b4db8a3
liuxiaoliang <15810140695@163.com>
2022-07-04 14:31:08 +0800

    Map

--- JavaProcess/src/main/java/com/ddcode/java/currentHashMap/Demo_2_Map.java
+++ JavaProcess/src/main/java/com/ddcode/java/currentHashMap/Demo_2_Map.java
@@ -9,6 +9,14 @@ import java.util.Set;
 @Slf4j(topic = "c.map")
 public class Demo_2_Map {
     public static void main(String[] args) {
+        test2();
+    }
+
+
+    /**
+     * map的遍历
+     */
+    public static void test1(){
         Map<String,String > map = new HashMap<>();
         map.put("name", "张安");
         map.put("age", "11");
@@ -19,4 +27,17 @@ public class Demo_2_Map {
             log.info("entry_key {}, entry_value {} , entry {}", entry.getKey(), entry.getValue(), entry.toString());
         });
     }
+
+    public static void test2(){
+        People people1 = People.builder().name("张三").age(1).build();
+        People people2 = People.builder().name("张三").age(1).build();
+
+        Map<People,String > map = new HashMap<>();
+        map.put(people1, "a");
+        map.put(people2, "b");
+
+        log.info("size {}" ,map.size());
+    }
+
+
 }
--- JavaProcess/src/main/java/com/ddcode/java/currentHashMap/People.java
+++ JavaProcess/src/main/java/com/ddcode/java/currentHashMap/People.java
@@ -1,5 +1,12 @@
 package com.ddcode.java.currentHashMap;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class People {
 
+    private String name;
+    private Integer age;
 }
