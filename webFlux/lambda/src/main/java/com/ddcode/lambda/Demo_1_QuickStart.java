aca8d3ada749ad2ee9a0004faffaef05b672ac95
liuxiaoliang <15810140695@163.com>
2022-12-01 09:53:11 +0800

    响应式编程

---
+++ webFlux/lambda/src/main/java/com/ddcode/lambda/Demo_1_QuickStart.java
@@ -0,0 +1,12 @@
+package com.ddcode.lambda;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j(topic = "c.Demo_1_QuickStart")
+public class Demo_1_QuickStart {
+
+    public static void main(String[] args) {
+        log.info("哈哈");
+    }
+}
