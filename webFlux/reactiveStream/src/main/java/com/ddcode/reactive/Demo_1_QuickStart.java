463519d23eac9e4584f5ea897623c24558254e81
liuxiaoliang <15810140695@163.com>
2023-02-07 17:57:57 +0800

    响应式编程之Flow

---
+++ webFlux/reactiveStream/src/main/java/com/ddcode/reactive/Demo_1_QuickStart.java
@@ -0,0 +1,11 @@
+package com.ddcode.reactive;
+
+/**
+ * 验证jdk11是否安装成功
+ */
+public class Demo_1_QuickStart {
+    public static void main(String[] args) {
+        var x = 10;
+        System.out.println(x);
+    }
+}
